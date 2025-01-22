e367376f22e4a698791bd4de7d310064274a9a9c
HugoSanche <hbmclaren@gmail.com>
2025-01-22 15:16:42 -0600

    other examples

--- CheckIfSortedArray.java
+++ CheckIfSortedArray.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class CheckIfSortedArray {
+public class CheckIfSortedArray {
+    public static void main(String[] args) {
+
+        int[] numbers = {1};
+
+        boolean result = checkIfSorted(numbers);
+        // result=numbers.length;
+        System.out.println(result);
+    }
+    public static boolean checkIfSorted(int[] array) {
+        //boolean flag = true;
+        for (int i = 1; i < array.length; i++) {
+            if(array[i]>=array[i-1]){
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
 }
--- Utilities.java
+++ Utilities.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Utilities {
+import java.util.Arrays;
+
+public class Utilities {
+    public static void main(String[] args) {
+        int[] array={5,4,9,1,8,2};
+
+        String str= Arrays.toString(array);
+        System.out.println(str);
+
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+
+        int[] copy=Arrays.copyOf(array,10);
+        System.out.println(Arrays.toString(copy));
+
+       Arrays.fill(array,3);
+        System.out.println(Arrays.toString(array));
+
+        boolean res=Arrays.equals(array,copy);
+        System.out.println(res);
+    }
 }
