c21e38a691149c619119199467e812a2d7bd2fdf
HugoSanche <hbmclaren@gmail.com>
2025-01-22 15:16:26 -0600

    Add example of get small number of array

--- SmallNumber.java
+++ SmallNumber.java
@@ -1,18 +1,16 @@
 public class SmallNumber {
 
     public static void main(String[] args) {
-        int[] numbers={7,4,3,7,1,5,0};
+        int[] numbers={7,4,3,9,1,5,0};
 
         int result=getSmallNumber(numbers);
         // result=numbers.length;
         System.out.println(result);
-
-
     }
     public static int getSmallNumber(int[] array){
         int small=array[0];
         for(int i=1; i<array.length;i++){
-            if(array[i]<small){
+            if(array[i]>small){
                 small=array[i];
             }
         }
