853325210a956d4e743932e9852df42864459f4d
HugoSanche <hbmclaren@gmail.com>
2025-01-22 15:15:14 -0600

    Add example of remove element of array

--- RemoveElement.java
+++ RemoveElement.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class RemoveElement {
+import java.util.Arrays;
+
+public class RemoveElement {
+    public static void main(String[] args) {
+        int[] array={5,3,6,8,1,0,3,4};
+        int element=4;
+        int[] result=removeElement(array,element);
+        System.out.println(Arrays.toString(result));
+    }
+    public static int[] removeElement(int[] array,int element){
+
+        int cont=0;
+        for(int i=0; i<array.length;i++){
+            if(array[i]==element){
+                   cont++;
+            }
+        }
+        int[] result=new int[array.length-cont];
+        int j=0;
+        for(int i=0; i<array.length;i++){
+            if(array[i]!=element){
+                result[j]=array[i];
+                j++;
+            }
+        }
+        return result;
+    }
 }
