abec3a82cba8be012570eb2a460cd204f9a5be79
HugoSanche <hbmclaren@gmail.com>
2025-01-22 15:15:54 -0600

    Add example of get second larges number of array

--- SecondLargestNumber.java
+++ SecondLargestNumber.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class SecondLargestNumber {
+public class SecondLargestNumber {
+    public static void main(String[] args) {
+        int[] numbers={0,40,3,5,1,6,7};
+
+        int result=getSecondLargestNumber(numbers);
+        // result=numbers.length;
+        System.out.println(result);
+    }
+    public static int getSecondLargestNumber(int[] numbers){
+        int temp=numbers[0];
+        int largest=numbers[0];
+        boolean flag=false;
+        for (int i=1; i<numbers.length; i++){
+            if(largest<numbers[i]){
+                largest=numbers[i];
+                flag=true;
+            }
+            if(temp<numbers[i] && flag && i>1){
+                temp=numbers[i];
+            }
+        }
+        return temp;
+    }
 }
